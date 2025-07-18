98cd573389907212a1b98e53f77df7692b6c986e
Ephrem0001 <ephrem8118@gmail.com>
2025-07-18 10:22:06 +0300

    Initial project setup with MySQL, Swagger, and REST config

--- alx_travel_app/urls.py
+++ alx_travel_app/urls.py
@@ -41,5 +41,6 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('', include('listings.urls')),  # You can create this file next
+    path('', include('listings.urls')),
 ]
+
