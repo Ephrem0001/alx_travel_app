44a538e8b9558cdbbb2caacc3423db731188667e
Ephrem0001 <ephrem8118@gmail.com>
2025-07-30 16:38:30 +0300

    Completed

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Listing(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price_per_night = models.DecimalField(max_digits=10, decimal_places=2)
+    location = models.CharField(max_length=255)
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+class Booking(models.Model):
+    listing = models.ForeignKey(Listing, related_name='bookings', on_delete=models.CASCADE)
+    guest_name = models.CharField(max_length=255)
+    check_in = models.DateField()
+    check_out = models.DateField()
+
+    def __str__(self):
+        return f"{self.guest_name} booking {self.listing.title}"
+
+
+class Review(models.Model):
+    listing = models.ForeignKey(Listing, related_name='reviews', on_delete=models.CASCADE)
+    reviewer_name = models.CharField(max_length=255)
+    rating = models.IntegerField()
+    comment = models.TextField()
+
+    def __str__(self):
+        return f"Review by {self.reviewer_name} - {self.rating}⭐"
+
+
